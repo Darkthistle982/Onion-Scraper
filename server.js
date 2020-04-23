8dd026ac2457003aa4f35cad2caf8050d8b6391b
Brian Allen <brianallen2401@gmail.com>
2020-04-23 12:20:17 -0600

    added eslint. setup initial server page. No routes yet. Created DB

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("views"));
+
+mongoose.connect("mongodb://localhost/onionScraper", { useNewUrlParser: true });
+
+//Routes//
+
+
+
+
+//Listener to Start the Server//
+app.listen(PORT, function() {
+    console.log("app running on port " + PORT);
+});
