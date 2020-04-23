2aac692af312eb9a906f2f7cf4694f9f8091732e
Brian Allen <brianallen2401@gmail.com>
2020-04-23 14:34:21 -0600

    added scrape logic. Only partially working. got initial index route to display

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
@@ -9,15 +10,46 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("views"));
+let exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("/views"));
 
 mongoose.connect("mongodb://localhost/onionScraper", { useNewUrlParser: true } );
 
 //Routes//
+app.get("/", function(request, response) {
+    response.render("index");
+});
 
+
+app.get("/scrape", function(request, response) {
+    axios.get("https://www.theonion.com/")
+    .then(function(response) {
+        let $ = cheerio.load(response.data);
+
+        $("article").each(function(i, element) {
+            let scrapedArticle = {};
+            scrapedArticle.title = $(this).children("h4").text();
+            scrapedArticle.summary = $(this).children("p").text();
+            scrapedArticle.link = $(this).children("a").attr("href");
+            
+            db.Article.create(scrapedArticle)
+            .then(function(dbArticle) {
+                console.log(dbArticle);
+            })
+            .catch(function(error) {
+               console.log(error);
+            })
+        })
+    })
+    response.send("Scrape Completed")
+})
 
 
 
