7fd42d910059a7bd1e7408a0c338a080111333a1
Brian Allen <brianallen2401@gmail.com>
2020-04-27 12:53:00 -0600

    added back to top function and styles. added comment modal. Not working

--- public/assets/app.js
+++ public/assets/app.js
@@ -27,4 +27,20 @@ $(document).ready(function () {
         throw error;
       });
   });
+
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+      $("#back2Top").fadeIn();
+    } else {
+      $("#back2Top").fadeOut();
+    }
+  });
+  $(document).ready(function() {
+    $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+    });
+  });
 });
